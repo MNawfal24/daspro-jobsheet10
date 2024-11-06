7c7dffc7f18a5cb1170010ca6ecec59b388b8955
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-11-06 21:50:50 +0700

    commit foreach

--- Bioskop19.java
+++ Bioskop19.java
@@ -16,9 +16,12 @@ public class Bioskop19 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
         
         System.out.println(penonton.length);
-        for (int i = 0; i< penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        for (String [] barisPenonton : penonton){
+            System.out.println("panjang baris : " + barisPenonton.length);
         }
-       
+       System.out.println("penonton pada baris ke-3 : ");
+       for(String i : penonton[2]){
+        System.out.println(i);
+       }
     }
 }
