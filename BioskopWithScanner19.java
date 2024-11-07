46944ba16000e22d1d1ab77cb1a86739a92fe814
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-11-07 08:09:25 +0700

    commit pertnyaan 2

--- BioskopWithScanner19.java
+++ BioskopWithScanner19.java
@@ -2,28 +2,52 @@ import java.util.Scanner;
 public class BioskopWithScanner19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int baris, kolom;
+        int baris, kolom, pilihan;
         String nama, next;
         String [][] penonton = new String[4][2];
 
 
         while(true) {
-            System.out.print("Masukkan Nama : ");
-            nama = sc.nextLine();
-            System.out.print(" Masukkan baris : ");
-            baris = sc.nextInt();
-            System.out.print(" Masukkan Kolom : ");
-            kolom = sc.nextInt();
+            System.out.println(" Selamat Datang di Bioskop, Menu apa yang ingin di pilih");
+            System.out.println("1. pesan tempat duduk");
+            System.out.println("2. daftar penonton");
+            System.out.println("0. Akhiri pemilihan");
+            System.out.println(" Masukkan pilihan dengan angka yang sesuai: ");
+            pilihan = sc.nextInt();
             sc.nextLine();
 
-             penonton [baris-1][kolom-1] = nama;
-             
-             System.out.print("input penonton lainnya ? (y/n)");
-             next = sc.nextLine();
-             
-             if(next.equalsIgnoreCase("n")) {
+            switch (pilihan){
+                case 0:
+                break ;
+                case 1:
+                    while(true){
+                        System.out.print("Masukkan nama : ");
+                        nama = sc.nextLine();
+                        System.out.print("Masukkan Baris : ");
+                        baris = sc.nextInt();
+                        System.out.print("Masukkan Kolom : ");
+                        kolom = sc.nextInt();
+
+                        sc.nextLine();
+
+                        penonton[(baris -  1)][(kolom - 1)] = nama;
+
+                        System.out.print("Input penonton lainya ? (y/n)");
+                        next = sc.nextLine();
+                        if(next.equalsIgnoreCase("n")){
+                            break;
+                        }
+                    }
+                 break;
+                 
+                 case 2:
+                    for (int i =0; i< penonton.length; i++) {
+                        System.out.println(" Pentonton baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]) + "\n");
+                    }
                 break;
-             }
+                default:
+                    System.out.println("Masukkan Pilihan Yang Tersedia !");
+            }
         }
     }
 }
