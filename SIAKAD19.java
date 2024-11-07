04ba961c4126192fc3456ae8611e41a40f6e3fb3
MNawfal24 <m.nawfal.mawla@gmail.com>
2024-11-07 10:22:28 +0700

    Commit tugass percobaan 4

--- SIAKAD19.java
+++ SIAKAD19.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 public class SIAKAD19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int[][] nilai = new int[4][3];
+        
+        System.out.print(" Masukkan jumlah Mahasiswa: ");
+        int mahasiswa = sc.nextInt();
+        System.out.print(" Masukkan Jumlah Mata Kuliah : ");
+        int matKul = sc.nextInt();
+        int[][] nilai = new int[mahasiswa][matKul];
 
         for (int i = 0; i < nilai.length; i++ ) {
-            System.out.println("Input nilai mahasiswa ke-" + (i+1));
+            System.out.println("Input nilai mahasiswa ke-" + (i+1))
             for(int j =0 ; j< nilai[i].length; j++) {
                 System.out.println("Nilai mata kuliah " + (j+1) + ": " );
                 nilai[i][j] = sc.nextInt();
@@ -16,8 +20,8 @@ public class SIAKAD19 {
             System.out.println("Input nilai mahasiswa ke-" + (i+1));
             double totalPerSiswa = 0;
             
-            for(int j = 0; j < nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+            for(int j = 0; j < nilai[i].length; J++) {
+                System.out.print("Nilai mata kuliah " + (j+1) + ": ")
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
